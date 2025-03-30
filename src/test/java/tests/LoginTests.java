d695eef6e910ece7498bf2b1fbe30ff8d4d8faa4
MedSveta <sveta1978medved@gmail.com>
2025-03-30 20:52:36 +0300

    Added LoginPage

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,4 +1,12 @@
 package tests;
 
-public class LoginTests {
+import manager.AppManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class LoginTests extends AppManager {
+    @Test
+    public void loginPositiveTest() {
+        new HomePage(getDriver()).clickBtnLogin();
+    }
 }
